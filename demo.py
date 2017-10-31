7a73d4de540b53f5915f8ce3686147571d7c7ffe
Christopher Chen <christoipher.chen@gmail.com>
2017-10-30 17:34:31 -0700

    Refactor for clarity.

--- demo.py
+++ demo.py
@@ -3,7 +3,8 @@ import repo_data
 
 def main():
     print("List of users who have solved Euler 001:")
-    print(', '.join(repo_data.who_solved(1)))
+    list_of_users = repo_data.who_solved(1)
+    print(', '.join(list_of_users))
 
 if __name__ == '__main__':
     main()
